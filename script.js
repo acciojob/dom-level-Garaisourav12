0558d81d4259b24f740b16bfb9f0c6aff9a0e3b7
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-12-24 21:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+// Get the element with the id 'level'
+const targetElement = document.getElementById('level');
+
+// Function to determine the DOM level of an element
+function getDomLevel(element) {
+  let level = 0;
+  let currentNode = element;
+
+  // Traverse parent nodes until reaching the root element (<html>)
+  while (currentNode !== null && currentNode.tagName !== 'HTML') {
+    level++;
+    currentNode = currentNode.parentNode;
+  }
+
+  return level;
+}
+
+// Get the DOM level of the target element
+const domLevel = getDomLevel(targetElement);
+
+// Display the result using alert()
+alert(`The level of the element is: ${domLevel}`);
